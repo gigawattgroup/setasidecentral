fb5b1b43635846c7ff40349c541fbb7034abfc0a
Brigitta B <user@Brigitta-MacBook-Pro.local>
2017-02-07 12:11:09 -0500

    About Page
    Update to About Page.

---
+++ builds/development/wp-content/themes/sac/template-parts/content-about.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Set_Aside_Central
+ */
+
+?>
+
+<div class="section-about overlay-black">
+	<div class="bcg" style="background-image:url('http://setaside.dev/wp-content/themes/sac/images/about-header.jpg')"></div>
+	<div class="overlay-black"></div>
+		<div class="container text-top">
+			<h1>About Set Aside</h1>
+			<p>Set Aside Central was formed for a singular purpose - to connect America’s small businessmen with the opportunities to swim with some of the biggest fishes and transform their companies.</p>
+		</div><!-- .container -->
+	</div><!-- .slide -->
+	<div class="page-links">Who We Are          |          Why Us</div>
+</div><!-- .section-carousel -->
+
+
+<div class="section-about">
+	<div class="bcg2" style="background-image:url('http://setaside.dev/wp-content/themes/sac/images/people-working.png')"></div>
+	<div class="container">
+		<div class="text-left equalheights">
+				<h2 class="entry-title">Who We Are</h2>
+				<p>There are billions of dollars in government contracts that HUBZone companies. <strong>Our mission is to get them in the hands of businesses that operate in some of the most vulnerable places in America.</strong> Whether in urban or rural areas, America’s small businesses deserve their shot at success.</p>
+				<p><strong>This is where we come in.</strong> Our software gives registered companies with opportunities in real time, giving them a vital edge in helping to land that big contract. We also provide help and support in registering themselves as a government certified contractor, with the target of ensuring every penny set aside for American small businesses makes it to America’s small businesses.</p>
+		</div>
+	</div>
+</div><!-- .section-hubzones -->
+
+<div class="section-map" style="background-image:url('http://setaside.dev/wp-content/themes/sac/images/flag_red.jpg')">
+	<video class="video-bg" autoplay loop="true" width="960" height="540">
+    <source type="video/mp4" src="http://setaside.dev/wp-content/themes/sac/images/flag.mp4">
+  </video>
+	<div class="overlay"></div>
+	<div class="container text-center">
+	<h2 class="entry-title">Why Us?</h2>
+	<div id="section-about-content">
+		<p class="white-box-text">We know what it means to be a small business trying to land government contracts within a HUBZone, because we have already done so. We know what to takes to succeed in what is a highly lucrative and competitive market. We know how landing even a small contract can make a huge difference, opening the door to even bigger opportunities in the future, and we are committed to helping our customers achieve success.  </p>
+		</div>
+	</div>
+</div><!-- .section-map -->
+
+<div class="section-about" style="background-image:url('http://setaside.dev/wp-content/themes/sac/images/flag-static.png')">
+	<div class="bcg"></div>
+	<div class="container">
+		<h2 class="entry-title">Why Us?</h2>
+		<div id="section-about-content" class="entry-content">
+			<div class="entry-content">
+				<p class="white-box-text">We know what it means to be a small business trying to land government contracts within a HUBZone, because we have already done so. We know what to takes to succeed in what is a highly lucrative and competitive market. We know how landing even a small contract can make a huge difference, opening the door to even bigger opportunities in the future, and we are committed to helping our customers achieve success.  </p>
+			</div>
+		</div>
+	</div>
+</div><!-- .section-about -->
